62a78ee35b856fda8319b54d81233558bd99fbf9
Hoseok Seong <shsshs0125@naver.com>
2023-07-07 21:43:41 +0900

    주식가격 done

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import programmers.주식가격;
+
+public class Main {
+    public static void main(String[] args) {
+        주식가격 x = new 주식가격();
+        int[] list = {1, 2, 3, 2, 3};
+        x.solution(list);
+    }
+}
